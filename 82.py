6420cb6ec305b666a5700712181cdb657be42fd9
HP <adithyakangatu001@gmqil.com>
2024-10-14 00:50:11 +0530

    14/10/2024

---
+++ 82.py
@@ -0,0 +1,15 @@
+#perform a case-insensitive sort of the list:
+
+thislist = ["banana","Orange","Kiwi","cherry"]
+thislist.sort(key = str. lower)
+
+print(thislist)
+
+#the key parameter is set to str.lower
+#which means that the list will be sorted based
+#on the lower case version of each string element
+
+#"banana". lower ()returns "banana"#
+#"Orange".lower()returns "Orange"
+#"kiwi" . lower () returns "kiwi"
+#"cherry".lower () returns "vherry"
